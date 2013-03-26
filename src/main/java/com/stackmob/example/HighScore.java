8b5add0fd180c2269735f40a22dfdf7dd63a6a01
Kyunhyun Son <sohnkh@gmail.com>
2013-03-26 16:54:57 +0900

    test5

--- src/main/java/com/stackmob/example/HighScore.java
+++ src/main/java/com/stackmob/example/HighScore.java
@@ -18,6 +18,7 @@ package com.stackmob.example;
 
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,7 +48,7 @@ public class HighScore implements CustomCodeMethod {
 
   @Override
   public List<String> getParams() {
-    return new ArrayList<String>();
+	  return Arrays.asList("username","score");
   }
 
   @Override
